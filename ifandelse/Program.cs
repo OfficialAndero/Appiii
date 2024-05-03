965c4e56526204f22b0e64ebf1c276f2a66c85ce
Opilane TTHK <anderoelias1@gmail.com>
2024-05-03 13:06:33 +0300

    ifandelse

---
+++ ifandelse/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Consoleifandelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If ja else nesting");
+
+            double y;
+
+            y = 9;
+
+            if (y < 20)
+            {       
+                if (y == 11)
+                {
+                    Console.WriteLine("Vastus on 11");
+                }
+                else
+                {
+                    Console.WriteLine("Vastus oli 0 kuni 10 ja 12 kuni 19");
+                }
+            }
+            else if (y == 20.5)
+            {
+                Console.WriteLine("Vastus on 20.5. Teine tingimus vastab tõele");
+            }
+            else if (y == 30)
+            {
+                Console.WriteLine("Vastus on 30. Kolmas tingimus vastab tõele");
+            }
+            else
+            {
+                Console.WriteLine("Mitte ükski väärtus, mida on kirja pandud koodi");
+            }
+
+
+        }
